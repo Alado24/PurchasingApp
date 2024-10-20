4a55cd83caa7428862e1af79fe839352b84f48e9
AdamK <ada@1.se>
2024-10-20 16:27:53 +0100

    Customer code

--- src/org/ulpgc/is1/model/Customer.java
+++ src/org/ulpgc/is1/model/Customer.java
@@ -1,4 +1,47 @@
 package org.ulpgc.is1.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Customer {
+    private final String name;
+    private final String surname;
+    private final Email email; // Cambiado de "account" a "email"
+    private final List<Address> deliveryAddresses = new ArrayList<>(); // Multiplicidad 1..*
+    private final List<Purchase> purchases = new ArrayList<>(); // Multiplicidad 1..*
+
+    public Customer(String name, String surname, Email email) { // Cambiado de "account" a "email"
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public Email getEmail() { // Cambiado de "getAccount" a "getEmail"
+        return email;
+    }
+
+    public void addDeliveryAddress(Address address) {
+        deliveryAddresses.add(address);
+    }
+
+    public List<Address> getDeliveryAddresses() {
+        return deliveryAddresses;
+    }
+
+    public void addPurchase(Purchase purchase) {
+        purchases.add(purchase);
+    }
+
+    public List<Purchase> getPurchases() {
+        return purchases;
+    }
+
 }
