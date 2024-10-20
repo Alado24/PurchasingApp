80447a6a83f1100546b9b2a0ea8df0e55fa47164
AdamK <ada@1.se>
2024-10-20 16:28:59 +0100

    Discount code

--- src/org/ulpgc/is1/model/Discount.java
+++ src/org/ulpgc/is1/model/Discount.java
@@ -1,4 +1,28 @@
 package org.ulpgc.is1.model;
 
+import java.time.LocalDate;
+
 public class Discount {
+    private final LocalDate from;
+    private final LocalDate to;
+    private final int percentage;
+
+    public Discount(LocalDate from, LocalDate to, int percentage) {
+        this.from = from;
+        this.to = to;
+        this.percentage = percentage;
+    }
+
+    public LocalDate getFrom() {
+        return from;
+    }
+
+    public LocalDate getTo() {
+        return to;
+    }
+
+    public int getPercentage() {
+        return percentage;
+    }
+
 }
