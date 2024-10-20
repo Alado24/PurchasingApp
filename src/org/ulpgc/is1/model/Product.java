8244666c4e8c56483c18d23ab59afe82e56bf0e8
AdamK <ada@1.se>
2024-10-20 16:33:12 +0100

    Product code

--- src/org/ulpgc/is1/model/Product.java
+++ src/org/ulpgc/is1/model/Product.java
@@ -1,4 +1,40 @@
 package org.ulpgc.is1.model;
 
 public class Product {
+    private final int id;
+    private final String name;
+    private final String description;
+    private final int price;
+    private ProductCategory category;
+
+    public Product(int id, String name, String description, int price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public ProductCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(ProductCategory category) {
+        this.category = category;
+    }
 }
