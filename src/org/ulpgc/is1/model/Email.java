483b0062ca1ad701509c387b612eb1e03c905ba3
AdamK <ada@1.se>
2024-10-20 16:30:36 +0100

    Email code

--- src/org/ulpgc/is1/model/Email.java
+++ src/org/ulpgc/is1/model/Email.java
@@ -1,4 +1,18 @@
 package org.ulpgc.is1.model;
 
 public class Email {
+    private final String account;
+
+    public Email(String account) {
+        this.account = account;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public boolean isValid() {
+        return account.contains("@");
+    }
+
 }
