c48239ffbf3e8780fb412f0cd68350c283ce1cc6
AdamK <ada@1.se>
2024-10-20 16:32:08 +0100

    Payment code

--- src/org/ulpgc/is1/model/Payment.java
+++ src/org/ulpgc/is1/model/Payment.java
@@ -1,4 +1,28 @@
 package org.ulpgc.is1.model;
 
+import java.time.LocalDate;
+
 public class Payment {
+    private final LocalDate date;
+    private final int amount;
+    private final String card;
+
+    public Payment(LocalDate date, int amount, String card) {
+        this.date = date;
+        this.amount = amount;
+        this.card = card;
+    }
+
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public String getCard() {
+        return card;
+    }
 }
