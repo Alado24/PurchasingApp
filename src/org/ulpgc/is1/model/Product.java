6acdca983080de8232f2772856e793ab3e63819a
Héctor <hectoraledel24@gmail.com>
2024-10-21 10:30:23 +0100

    Implemented Discount type field in class Product

--- src/org/ulpgc/is1/model/Product.java
+++ src/org/ulpgc/is1/model/Product.java
@@ -6,13 +6,15 @@ public class Product {
     private String description;
     private int price;
     private ProductCategory category;
+    private Discount discount;
 
-    public Product(int id, String name, String description, int price, ProductCategory category) {
+    public Product(int id, String name, String description, int price, ProductCategory category, String dateFrom, String dateTo, int percentage) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.price = price;
         this.category = category;
+        this.discount = new Discount(dateFrom, dateTo, percentage);
     }
 
     public int getId() {
@@ -49,5 +51,13 @@ public class Product {
 
     public void setCategory(ProductCategory category) {
         this.category = category;
+    }
+
+    public Discount getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(String dateFrom, String dateTo, int percentage) {
+        this.discount = new Discount(dateFrom, dateTo, percentage);
     }
 }
