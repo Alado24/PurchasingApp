d86c0dd5e53ad74e5458586c9b0960101cec1727
Héctor <hectoraledel24@gmail.com>
2024-10-21 10:06:33 +0100

    Added setters for class Product and added category to the constructor parameters.

--- src/org/ulpgc/is1/model/Product.java
+++ src/org/ulpgc/is1/model/Product.java
@@ -2,16 +2,17 @@ package org.ulpgc.is1.model;
 
 public class Product {
     private final int id;
-    private final String name;
-    private final String description;
-    private final int price;
+    private String name;
+    private String description;
+    private int price;
     private ProductCategory category;
 
-    public Product(int id, String name, String description, int price) {
+    public Product(int id, String name, String description, int price, ProductCategory category) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.price = price;
+        this.category = category;
     }
 
     public int getId() {
@@ -22,12 +23,24 @@ public class Product {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getDescription() {
         return description;
     }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public int getPrice() {
         return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
     }
 
     public ProductCategory getCategory() {
