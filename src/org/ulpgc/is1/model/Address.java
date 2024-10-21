c1d7af02557a09dd1ac810f19e73ebea95e10322
Héctor <hectoraledel24@gmail.com>
2024-10-21 10:45:26 +0100

    Added setters for class Address

--- src/org/ulpgc/is1/model/Address.java
+++ src/org/ulpgc/is1/model/Address.java
@@ -1,10 +1,10 @@
 package org.ulpgc.is1.model;
 
 public class Address {
-    private final String street;
-    private final int number;
-    private final int postalCode;
-    private final String city;
+    private String street;
+    private int number;
+    private int postalCode;
+    private String city;
 
     public Address(String street, int number, int postalCode, String city) {
         this.street = street;
@@ -17,15 +17,31 @@ public class Address {
         return street;
     }
 
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
     public int getNumber() {
         return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
     }
 
     public int getPostalCode() {
         return postalCode;
     }
 
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+
     public String getCity() {
         return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
     }
 }
